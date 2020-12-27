81747a4859cdcd9ee3d373c53a41a8cc5c5936be
Manfred Baur <manfred.baur@web.de>
2020-12-27 16:53:45 +0100

    format fehler behoben; bit-manipulationen korrigiert

--- smbus/testExpander.py
+++ smbus/testExpander.py
@@ -7,8 +7,8 @@ bus = smbus.SMBus(1) # Rev 2 Pi
 DEVICE = 0x20 # Device Adresse (A0-A2)
 IODIRA = 0x00 # Pin Register fuer die Richtung
 IODIRB = 0x01 # Pin Register fuer die Richtung
-OLATB = 0x15 # Register fuer Ausgabe (GPB)
 GPIOA = 0x12 # Register fuer Eingabe (GPA)
+GPIOB = 0x15 # Register fuer Ausgabe (GPB)
  
 # Definiere GPA Pin 7 als Input (10000000 = 0x80)
 # Binaer: 0 bedeutet Output, 1 bedeutet Input
@@ -18,25 +18,26 @@ bus.write_byte_data(DEVICE,IODIRA,0x00)
 bus.write_byte_data(DEVICE,IODIRB,0x00)
  
 # Setze alle 7 Output bits auf 0
-bus.write_byte_data(DEVICE,OLATA,0)
-bus.write_byte_data(DEVICE,OLATB,0)
+bus.write_byte_data(DEVICE,GPIOA,0)
+bus.write_byte_data(DEVICE,GPIOB,0)
  
 portA=0 
  
  # which=0..7
- def onA(which):
-   bit=(1 << which)
-   portA=portA | bit
-   bus.write_byte_data(DEVICE,OLATA,portA)
+def onA(which):
+    global portA
+    bit=(1 << which)
+    portA=portA | bit
+    bus.write_byte_data(DEVICE,GPIOA,portA)
    
- def offA(which):
-   bit=(1 << which)
-   portA=portA & (~bit)
-   bus.write_byte_data(DEVICE,OLATA,portA)
+def offA(which):
+    global portA
+    bit=(1 << which)
+    portA=portA & (~bit)
+    bus.write_byte_data(DEVICE,GPIOB,portA)
  
 for i in range(0,4):
   onA()
   time.sleep(0.5)
   offA()
   time.sleep(0.5)
-  
